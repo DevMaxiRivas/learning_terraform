8047c8158bba0dc2e83fb0a09e42cad0e7060b44
DevMaxiRivas <maximilianorivas01@gmail.com>
2026-07-28 20:01:27 -0300

    Role association was performed on an EC2 instance

--- iam/01_roles_policies_iam/main.tf
+++ iam/01_roles_policies_iam/main.tf
@@ -7,23 +7,23 @@ resource "aws_s3_bucket" "test_bucket" {
   }
 }
 
-# Reto 1: Crea un usuario que solo pueda ver (Read-Only) los buckets de S3, pero que no pueda crear uno nuevo
+# Challenge 1: Create a user who can only view (Read-Only) S3 buckets, but cannot create a new one
 resource "aws_iam_user" "user" {
-    name = "read_only_s3_user"
+    name = var.user_name
 }
 
 resource "aws_iam_access_key" "user" {
   user = aws_iam_user.user.name
 }
 
-# With AmazonS3ReadOnlyAccess for all buckets
+## With AmazonS3ReadOnlyAccess for all buckets
 resource "aws_iam_user_policy_attachment" "attachment" {
   user       = aws_iam_user.user.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
 
 
-# With custom policies for s3_bucket_name
+## With custom policies for s3_bucket_name
 data "aws_iam_policy_document" "policy_object" {
     statement {
         actions   = [
@@ -38,7 +38,7 @@ data "aws_iam_policy_document" "policy_object" {
 }
 
 resource "aws_iam_policy" "policy" {
-  name   = "read-only_${var.s3_bucket_name}"
+  name   = var.policy_name
   description = "Policy read only for ${var.s3_bucket_name}"
   policy = data.aws_iam_policy_document.policy_object.json
 }
@@ -48,9 +48,86 @@ resource "aws_iam_user_policy_attachment" "test-attach" {
   policy_arn = aws_iam_policy.policy.arn
 }
 
-# Reto 2: Crea un Rol y asígnalo a una instancia de EC2 para que el servidor pueda listar archivos de S3 sin que tú tengas que configurar credenciales manualmente dentro del servidor.
+## Validation
+# aws iam list-attached-user-policies --user-name read_only_s3_user
 
-resource "aws_iam_role" "test_role" {
-  name = "read-only_${var.s3_bucket_name}_role"
+# Challenge 2: Create a role and assign it to an EC2 instance so that the server can list files from S3 without you having to manually configure credentials on the server.
+
+## Only for bucket test_role
+resource "aws_iam_role" "role" {
+  name = var.role_name
   assume_role_policy = data.aws_iam_policy_document.policy_object.json
 }
+
+## Creating Profile to attach to new EC2 instance
+resource "aws_iam_instance_profile" "profile" {
+  name = var.profile_name
+  role = var.role_name
+}
+
+## Fetch the latest official Amazon AMI
+data "aws_ami" "ami_amazon" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+## Creating an EC2 instance
+resource "aws_instance" "ec2_instance" {
+  ami           = data.aws_ami.ami_amazon.id
+  instance_type = "t3.micro"
+  iam_instance_profile = var.profile_name
+
+
+  tags = {
+    Name = "EC2Instance"
+  }
+}
+
+## Validation
+### Check attachment profile
+# aws ec2 describe-instances \
+#     --instance-ids [instanceID] \
+#     --query "Reservations[*].Instances[*].IamInstanceProfile"
+
+### Check roles attachment to profile
+# aws iam get-instance-profile --instance-profile-name read-only_test_bucket_role
+
+
+# Challenge 3: Configure a policy that only allows access to the AWS console if the request comes from a specific IP address (your home).
+data "aws_iam_policy_document" "policy_object_deny_all_ips" {
+    statement {
+        actions   = ["*"]
+        effect = "Deny"
+        resources = ["*"]
+        # condition {
+        #   notIpAddress: 
+        # }
+    }
+}
+
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Sid": "RestrictToOfficeIP",
+#       "Effect": "Deny",
+#       "Action": "*",
+#       "Resource": "*",
+#       "Condition": {
+#         "NotIpAddress": {
+#           "aws:SourceIp": ["203.0.113.111/32"]
+#         }
+#       }
+#     }
+#   ]
+# }
--- iam/01_roles_policies_iam/variables.tf
+++ iam/01_roles_policies_iam/variables.tf
@@ -4,9 +4,32 @@ variable aws_region {
   description = "AWS region"
 }
 
+variable "user_name" {
+  type = string
+  default = "read_only_s3_user"
+  description = "AWS New User"
+}
 
 variable "s3_bucket_name" {
   type = string
   default = "test_bucket"
   description = "AWS Bucket Name"
+}
+
+variable "policy_name" {
+  type = string
+  default = "read-only_test_bucket"
+  description = "AWS Policy Name"
+}
+
+variable "role_name" {
+  type = string
+  default = "read-only_test_bucket_role"
+  description = "AWS Role Name"
+}
+
+variable "profile_name" {
+  type = string
+  default = "read-only_test_bucket_role_profile"
+  description = "AWS Profile Name"
 }
